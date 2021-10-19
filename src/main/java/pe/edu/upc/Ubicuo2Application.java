fe5f42f674f8bc0bec674c22dfdb371664107233
jorgericard <jr.sebas7@gmail.com>
2021-10-18 22:29:36 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/Ubicuo2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ubicuo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ubicuo2Application.class, args);
+	}
+
+}
