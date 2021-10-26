963c71162825f5a04dd24641a9da5b8333a5b3af
Josef Romero Florida <u201910655@upc.edu.pe>
2021-10-25 20:02:35 -0500

    TipoUsuarioDefinitivo

--- src/main/java/pe/edu/upc/controller/TipoUsuarioController.java
+++ src/main/java/pe/edu/upc/controller/TipoUsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.support.SessionStatus;
@@ -44,7 +45,7 @@ public class TipoUsuarioController
 	}
 	
 	@PostMapping("/save")
-	public String saveMarca(@Valid TipoUsuario tipousuario, BindingResult result, Model model, SessionStatus status) throws Exception 
+	public String saveMarca(@ModelAttribute("tipousuario") @Valid TipoUsuario tipousuario, BindingResult result, Model model, SessionStatus status) throws Exception 
 	{
 		if (result.hasErrors()) 
 		{
