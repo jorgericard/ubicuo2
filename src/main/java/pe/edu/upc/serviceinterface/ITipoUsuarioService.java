8ecee57ed5b8c272021b4ad70095eda332f2576e
Josef Romero Florida <u201910655@upc.edu.pe>
2021-11-09 09:47:19 -0500

    TipoUsuarioEliminar

--- src/main/java/pe/edu/upc/serviceinterface/ITipoUsuarioService.java
+++ src/main/java/pe/edu/upc/serviceinterface/ITipoUsuarioService.java
@@ -1,12 +1,14 @@
 package pe.edu.upc.serviceinterface;
 
 import java.util.List;
+import java.util.Optional;
 
 import pe.edu.upc.entities.TipoUsuario;
 
 public interface ITipoUsuarioService 
 {
 	public Integer insert(TipoUsuario TipoUsuario);
-	
 	List<TipoUsuario> list();
+	public void delete(int cTipoUsuario);	
+	Optional<TipoUsuario> listId(int cTipoUsuario);
 }
